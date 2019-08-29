ec5faa9655de2468b5cae13a5730e4eb8a0ccdd1
Curtis Baucom <curtbaucom@aol.com>
2019-08-28 21:30:16 -0400

    8/28 updates

---
+++ PostgreSQL_db/Queries/create_tables.sql
@@ -0,0 +1,75 @@
+--create tables:  trips_raw, trips, temp_sataions, stations, weather, and dates
+
+CREATE TABLE trips_raw (
+  trip_duration numeric,
+  start_timestamp timestamp without time zone,
+  stop_timestamp timestamp without time zone,
+  start_station_id integer,
+  start_station_name text,
+  start_station_latitude numeric,
+  start_station_longitude numeric,
+  end_station_id integer,
+  end_station_name text,
+  end_station_latitude numeric,
+  end_station_longitude numeric,
+);  
+	
+CREATE TABLE trips (
+  id serial primary key,
+  start_date date,
+  stop_date date,	
+  start_time time,
+  stop_time time,
+  start_timestamp timestamp without time zone,
+  stop_timestamp timestamp without time zone,
+  trip_dur_sec numeric,
+  trip_dur_min numeric,	
+  start_station_id integer,
+  end_station_id integer,
+  bike_id integer,
+  user_type text,
+  birth_year integer,	
+  gender integer
+);
+
+
+
+CREATE TABLE temp_stations (
+  id serial primary key,
+  external_id integer not null,
+  name text,
+  latitude numeric,
+  longitude numeric
+);
+
+CREATE TABLE stations (
+  id serial primary key,
+  external_id integer not null,
+  name text,
+  latitude numeric,
+  longitude numeric
+);
+
+CREATE TABLE weather (
+  station_id text,
+  station_name text,
+  date date primary key,
+  precipitation numeric,
+  snow_depth numeric,
+  snowfall numeric,
+  max_temperature numeric,
+  min_temperature numeric,
+  average_wind_speed numeric
+);
+
+CREATE TABLE dates (
+  date date primary key,
+  month_num numeric,
+  dofm numeric,
+  day text,	
+  week numeric,
+  month text,
+  mmm_yy text,
+  quarter text,
+  year numeric
+);
