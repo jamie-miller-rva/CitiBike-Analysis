864a0655ceba7f7d5f58ed6bc84c8a49840fdb50
Nush Sash <anusha.rama@gmail.com>
2019-08-29 22:36:17 -0400

    browser buildup code with graphs

---
+++ Browser/data/sample_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Import data from csv
+tripdata = pd.read_csv('trips.csv')
+
+trace1 = go.Scatter(
+    x=tripdata['']
+)
---
+++ Browser/js/sample_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Import data from csv
+df = pd.read_csv('trips.csv')
